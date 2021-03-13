9e1321e3e68f95d969d9c17e8f05d1297d6213db
Fulano de Tal <raziel_libertino@hotmail.com>
2021-03-13 12:08:47 -0400

    gerando html para email

--- src/services/SendMainService.ts
+++ src/services/SendMainService.ts
@@ -1,4 +1,7 @@
 import nodemailer, { Transporter } from 'nodemailer';
+import { resolve } from 'path';
+import handlebars from 'handlebars';
+import fs from 'fs';
 
 class SendMailService {
     private client: Transporter
@@ -18,10 +21,20 @@ class SendMailService {
 
     }
     async execute(to: string, subject: string, body: string) {
+        const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");
+        const templateFileContent = fs.readFileSync(npsPath).toString("utf8");
+
+        const mailTemplateParse = handlebars.compile(templateFileContent);
+
+        const html = mailTemplateParse({
+            name: to,
+            title: subject,
+            description: body
+        })
         const message = await this.client.sendMail({
             to,
             subject,
-            html: body,
+            html,
             from: "NPS <noreplay@nps.com.br>"
         })
 
