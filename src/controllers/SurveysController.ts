e55e49cf10f2485035602adcaab05c4953f149bd
Fulano de Tal <raziel_libertino@hotmail.com>
2021-03-12 21:56:29 -0400

    migration repo and initial controller surveys

---
+++ src/controllers/SurveysController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+
+class SurveysController {
+    async create(request: Request, response: Response) {
+        
+    }
+}
+
+export { SurveysController }
